5d9e2a719374d6c98721b7dbfa6fcad700660b8d
Nandan1998 <52921706+Nandan1998@users.noreply.github.com>
2019-11-08 20:46:41 +0530

    Add files via upload
    Source codes for creating database file,input student details to database,extracting number wise ,monthly as well as yearly details of students included... BAT files added for each code as well.

---
+++ import_to_db.bat
@@ -0,0 +1,7 @@
+@ECHO ON 
+REM Runs both my project scripts
+
+C:\Users\Raghu\AppData\Local\Programs\Python\Python37-32\Python.exe C:\Users\Raghu\Desktop\project\input.py
+ECHO Attendance imporetd to Database Successfully........
+
+PAUSE
---
+++ student_extract.bat
@@ -0,0 +1,7 @@
+@ECHO ON 
+REM Runs both my project scripts
+
+C:\Users\Raghu\AppData\Local\Programs\Python\Python37-32\Python.exe C:\Users\Raghu\Desktop\project\student_extract.py
+ECHO Attendance exported to student_details_for_a_month.csv Successfully........
+
+PAUSE
